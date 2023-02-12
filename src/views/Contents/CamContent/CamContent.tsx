b071c21bafcd650fa582b581438d7ec4510e6a83
olive-su <1466su@naver.com>
2023-02-13 00:11:35 +0900

    Refactor: 비디오 녹화 useState 적용

--- src/views/Contents/CamContent/CamContent.tsx
+++ src/views/Contents/CamContent/CamContent.tsx
@@ -36,14 +36,14 @@ export default function CamContent() {
             <span>
                 <hr />
             </span>
-            {/* <StyleContent>
+            <StyleContent>
                 <Sidebar />
-                <WebcamContent> */}
-            {/* <WebcamPage> */}
-            <WebCamPage />
-            {/* </WebcamPage> */}
-            {/* </WebcamContent> */}
-            {/* </StyleContent> */}
+                {/* <WebcamContent> */}
+                <WebCamPage />
+                {/* <WebcamPage> */}
+                {/* </WebcamPage> */}
+                {/* </WebcamContent> */}
+            </StyleContent>
             <span>
                 <hr style={{ marginTop: '-1px' }} />
             </span>
--- src/views/webCam/index.tsx
+++ src/views/webCam/index.tsx
@@ -1,15 +1,63 @@
 /* eslint-disable */
 import './index.css';
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import axios from 'axios';
 import * as faceapi from 'face-api.js';
 import constraints from '../../common/constraints'; // CHECK
+import styled, { keyframes } from 'styled-components';
+import { TbLoader } from 'react-icons/tb';
+
+const rotate = keyframes`
+  from {
+      transform: rotate(0deg);
+    }
+    
+    to { transform: rotate(360deg);
+    }
+    `;
 
 // TODO : any 체크하기
 
-// 웹캠이 시작 중인지 확인
-let camStart = false;
+const Rotate = styled.div`
+    display: inline-block;
+    animation: ${rotate} 2s linear infinite;
+    padding: 2rem 1rem;
+    font-size: 1.2rem;
+`;
 
+const OnButton = styled.button`
+    color: #8a1441;
+    font-size: 1em;
+    width: 80px;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid #8a1441;
+    border-radius: 3px;
+    font-family: GangwonEduPowerExtraBoldA;
+    &:active,
+    &:hover,
+    &:focus {
+        background: var(--button-hover-bg-color, white);
+    }
+`;
+
+const OffButton = styled.button`
+    color: #2679cc;
+    font-size: 1em;
+    width: 80px;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid #2679cc;
+    font-family: GangwonEduPowerExtraBoldA;
+    border-radius: 3px;
+    &:active,
+    &:hover,
+    &:focus {
+        background: var(--button-hover-bg-color, white);
+    }
+`;
+
+let camFlag = false; // 웹 캠 작동 여부
 let recordFlag = false; // 녹화 중인지 아닌지
 let recentRecordTime: number;
 let recordInfo: { maxValue: any; label: any; count: number; startTime: any; maxTime: any };
@@ -30,8 +78,45 @@ function WebCamPage() {
     const wrapRef = useRef<any>(null);
     const videoRef = useRef<any>(null);
 
-    const [isStartDetect, setIsStartDetect] = useState(false);
-    const [modelsLoaded, setModelsLoaded] = useState(false);
+    const [camStarted, setCamStarted] = useState(false);
+    const [modelLoaded, setModelLoaded] = useState(false);
+
+    useEffect(() => {
+        // 비디오
+        navigator.mediaDevices
+            .getUserMedia(constraints)
+            .then((stream) => {
+                if (videoRef.current) {
+                    if (camStarted) {
+                        videoRef.current.srcObject = stream;
+                        setModelLoaded(true);
+                    } else {
+                        setModelLoaded(false);
+                        videoRef.current.srcObject = null;
+                    }
+                }
+            })
+            .catch((err) => console.error(err));
+    }, [camStarted]);
+
+    useEffect(() => {
+        // 모델 로드
+        const MODEL_URL = '/models';
+
+        Promise.all([
+            faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+            faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+            faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+            faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
+            // 사용자 이미지 Face Detection
+            faceapi.nets.ssdMobilenetv1.loadFromUri(MODEL_URL),
+            faceapi.nets.ageGenderNet.loadFromUri(MODEL_URL),
+        ])
+            .then(() => {
+                console.log('모델 로드 완료');
+            })
+            .catch((err) => console.error(err));
+    }, [modelLoaded]);
 
     // 라벨링 할 인물 이미지 로컬에서 가져오기
     const loadImage = async () => {
@@ -56,23 +141,21 @@ function WebCamPage() {
         const canvas = faceapi.createCanvasFromMedia(videoRef.current as HTMLVideoElement);
         if (wrapRef.current !== null) wrapRef.current.append(canvas);
 
-        // 다운로드할 영상 변수 생성
-        const mediaStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-
-        // 새로운 영상 객체 생성
-        const mediaRecorder = new MediaRecorder(mediaStream);
+        const mediaStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true }); // 다운로드할 영상 변수 생성
+        const mediaRecorder = new MediaRecorder(mediaStream); // 새로운 영상 객체 생성
         mediaRecorder.ondataavailable = handleDataAvailable;
 
         let displaySize = { width: constraints.video.width, height: constraints.video.height };
-        // 영상 사이즈를 canvas에 맞추기 위한 설정
+
         if (videoRef.current) {
+            // 영상 사이즈를 canvas에 맞추기 위한 설정
             displaySize = {
                 width: videoRef.current.width,
                 height: videoRef.current.height,
             };
         }
-        // canvas 사이즈를 맞춤
-        faceapi.matchDimensions(canvas, displaySize);
+
+        faceapi.matchDimensions(canvas, displaySize); // canvas 사이즈를 맞춤
 
         // 로컬 대조 이미지 가져오기
         const labeledFaceDescriptors = await loadImage();
@@ -109,14 +192,13 @@ function WebCamPage() {
 
                     faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
 
-                    // TODO
                     const { neutral, ...otherDetection }: Record<string, any> = detection.expressions;
 
                     expression.value = Math.max(...Object.values(otherDetection));
                     expression.label = Object.keys(otherDetection).find((key) => otherDetection[key] === expression.value); // 현재 최대 수치 감정 종류 가져오기
                     expression.time = Date.now();
                 });
-            return expression; // TODO
+            return expression;
         };
 
         const loop = async () => {
@@ -156,7 +238,7 @@ function WebCamPage() {
                 try {
                     mediaRecorder.stop();
                     RecordInit();
-                    console.log('녹화중지');
+                    console.log('녹화 중지');
                 } catch (err) {
                     console.log(err);
                 }
@@ -164,80 +246,16 @@ function WebCamPage() {
         }, 10000);
     };
 
-    // 모델에 따른 얼굴 감지 시작
-    const startDetecting = async () => {
-        // model load
-        const loadModels = async () => {
-            const MODEL_URL = '/models';
-
-            Promise.all([
-                faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
-                faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
-                faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
-                faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
-                // video 에서 로드된 이미지 매칭 시 아래 모델이 필요 함.
-                faceapi.nets.ssdMobilenetv1.loadFromUri(MODEL_URL),
-                faceapi.nets.ageGenderNet.loadFromUri(MODEL_URL),
-            ]).then(() => {
-                setModelsLoaded(true);
-                if (camStart === false) {
-                    camStart = true;
-                    startVideo();
-                }
-            });
-        };
-
-        loadModels();
-    };
-
-    // 영상 권한 요청
-    const startVideo = () => {
-        setIsStartDetect(true);
-
-        navigator.mediaDevices
-            .getUserMedia(constraints)
-            .then((stream) => {
-                if (videoRef.current) videoRef.current.srcObject = stream;
-            })
-            .catch((err) => console.error(err));
-
-        console.log('영상 시작');
-    };
-
-    // 모델에 따른 얼굴 감지 중지
-    const stopDetecting = async () => {
-        if (camStart === true) {
-            setModelsLoaded(false);
-            camStart = false;
-
-            setIsStartDetect(false);
-
-            navigator.mediaDevices
-                .getUserMedia(constraints)
-                .then((stream) => {
-                    if (videoRef.current) videoRef.current.srcObject = null;
-                })
-                .catch((err) => console.error(err));
-        }
-
-        console.log('영상 중지');
-    };
-
-    // 다운로드 된 영상을 배열로 저장
-    const recordedChunks: any[] = [];
-
     function handleDataAvailable(event: any) {
+        const recordedChunks: any[] = [];
         if (event.data.size > 0) {
-            // 새로운 영상을 저장하면 배열에 push
-            // recordedChunks.push(event.data);
-
-            download();
-            // 다운로드가 끝나면 다음 영상 다운으로 위해 배열에서 pop
-            // recordedChunks.pop();
+            recordedChunks.push();
+            download(recordedChunks);
+            recordedChunks.pop();
         }
     }
 
-    function download() {
+    function download(recordedChunks: any[]) {
         const blob = new Blob(recordedChunks, {
             type: 'video/webm',
         });
@@ -251,19 +269,19 @@ function WebCamPage() {
         window.URL.revokeObjectURL(url);
     }
 
-    // function uploadToS3Bucket() {
-    // 녹화 영상 저장 이름 조합
-    //    recordSave = `${currentTime}`;
+    // function uploadToS3Bucket(recordedChunks: any[]) {
+    //     // 녹화 영상 저장 이름 조합
+    //     recordSave = `${currentTime}`;
 
-    //    const file = new File(recordedChunks, `${recordSave}.webm`);
+    //     const file = new File(recordedChunks, `${recordSave}.webm`);
 
-    //    const formData = new FormData();
-    //    const expressionData = {
-    //        user_id: userId,
-    //        expression: startExpression,
-    //        accuracy: recordExpressionMaxValue,
-    //        time: recordExpressionMaxtime,
-    //    };
+    //     const formData = new FormData();
+    //     const expressionData = {
+    //         user_id: userId,
+    //         expression: startExpression,
+    //         accuracy: recordExpressionMaxValue,
+    //         time: recordExpressionMaxtime,
+    //     };
 
     //     formData.append('file', file);
     //     formData.append('expressionData', JSON.stringify(expressionData));
@@ -288,19 +306,31 @@ function WebCamPage() {
 
     return (
         <div>
-            <h2>Face-Api Video Test</h2>
-            <ul>
-                <li>model loaded: {modelsLoaded.toString()}</li>
-            </ul>
-            {wrapRef !== undefined && (
-                <div ref={wrapRef} id="wrap">
-                    <video ref={videoRef} autoPlay muted onPlay={onPlay} width={constraints.video.width} height={constraints.video.height} />
+            <h2>Recording My DAY</h2>
+
+            <div
+                ref={wrapRef}
+                id="wrap"
+                style={{
+                    borderStyle: 'none',
+                    width: constraints.video.width,
+                    height: constraints.video.height,
+                }}
+            >
+                <div>
+                    {camStarted ? (
+                        <video ref={videoRef} autoPlay muted onPlay={onPlay} width={constraints.video.width} height={constraints.video.height} />
+                    ) : (
+                        <Rotate>
+                            <TbLoader size="50" style={{ padding: 0 }} />
+                        </Rotate>
+                    )}
                 </div>
-            )}
-
-            <button onClick={startDetecting}>영상 시작</button>
-
-            <button onClick={stopDetecting}>영상 중지</button>
+            </div>
+            <div>
+                <OnButton onClick={() => setCamStarted(true)}>ON</OnButton>
+                <OffButton onClick={() => setCamStarted(false)}>OFF</OffButton>
+            </div>
         </div>
     );
 }
