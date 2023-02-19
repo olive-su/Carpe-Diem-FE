064b63f631be0091e40b75752cbad6da9eeff65b
olive-su <1466su@naver.com>
2023-02-19 19:46:31 +0900

    Design: webrtc 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import LibraryContent from './views/Contents/LibraryContent/LibraryContent';
 import VideoContent from './views/Contents/VideoContent/VideoContent';
 import CamContent from './views/Contents/CamContent/CamContent';
+import OnCamera from './views/Camera/OnCamera';
 import './styles/App.css';
 import { createGlobalStyle } from 'styled-components';
 import AlbumSinglePage from './views/Album/AlbumSinglePage';
@@ -15,6 +16,7 @@ function App() {
             <Routes>
                 <Route>
                     <Route path="/" element={<CamContent />} />
+                    <Route path="/camera" element={<OnCamera />} />
                     <Route path="/video" element={<VideoContent />} />
                     <Route path="/video/:cardId" element={<AlbumSinglePage />} />
                     <Route path="/album" element={<LibraryContent />} />
---
+++ src/views/Camera/onCamera.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function onCamera() {
+    return <div>onCamera</div>;
+}
