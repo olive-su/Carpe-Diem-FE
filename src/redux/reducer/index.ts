be843a1f8290d472d7586d45753d2643f7505166
olive-su <1466su@naver.com>
2023-02-27 21:22:46 +0900

    Refactor: 앨범 API 로직 saga 전환

--- src/redux/reducer/index.ts
+++ src/redux/reducer/index.ts
@@ -1,13 +1,16 @@
 import { combineReducers } from 'redux';
 import notiReducer from './notiReducer';
-import { albumListReducer, albumReducer } from './albumReducer';
 import { authReducer, usimReducer } from './authReducer';
+import { albumListReducer, albumReducer } from './albumReducer';
+import { cardListReducer, cardReducer } from './cardReducer';
 
 const rootReducer = combineReducers({
     auth: authReducer,
     usim: usimReducer,
     albumList: albumListReducer,
     album: albumReducer,
+    cardList: cardListReducer,
+    card: cardReducer,
     noti: notiReducer,
 });
 
