c2d4d6ba1fa479a36eaf01570e49bcba3ba88d42
nickhealthy <alskadmlcraz1@gmail.com>
2023-02-28 13:49:19 +0900

    Feat: 페이지네이션-카드(무한 스크롤 추가)

--- src/redux/saga/albumSaga.ts
+++ src/redux/saga/albumSaga.ts
@@ -9,11 +9,18 @@ import {
     ALBUM_LOADING_REQUEST,
     ALBUM_LOADING_SUCCESS,
     ALBUM_LOADING_FAILURE,
+    ALBUM_CREATE_REQUEST,
+    ALBUM_CREATE_SUCCESS,
+    ALBUM_CREATE_FAILURE,
+    ALBUM_UPDATE_REQUEST,
+    ALBUM_UPDATE_SUCCESS,
+    ALBUM_UPDATE_FAILURE,
+    ALBUM_DELETE_REQUEST,
+    ALBUM_DELETE_SUCCESS,
+    ALBUM_DELETE_FAILURE,
 } from '../types';
 
-/** 앨범 리스트 로드 */
-// CHECK userId
-
+/* 앨범 리스트 로드 */
 const albumListLoadAPI: any = (data: any) => {
     return axios({
         method: 'get',
@@ -68,13 +75,110 @@ function* albumLoad(action: any): any {
 function* watchAlbumLoad() {
     yield takeEvery(ALBUM_LOADING_REQUEST, albumLoad);
 }
+/* 앨범 생성 */
+const albumCreateAPI = (data: any) => {
+    return axios({
+        method: 'post',
+        url: `http://${config.server.host}:${config.server.port}/album`,
+        withCredentials: true,
+        data: data,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+};
+
+function* albumCreate(action: any): any {
+    try {
+        const result = yield call(albumCreateAPI, action.payload);
+        console.log(result.data);
+
+        yield put({
+            type: ALBUM_CREATE_SUCCESS,
+            payload: result.data,
+        });
+    } catch (e) {
+        yield put({
+            type: ALBUM_CREATE_FAILURE,
+            payload: e,
+        });
+    }
+}
+
+function* watchAlbumCreate() {
+    yield takeEvery(ALBUM_CREATE_REQUEST, albumCreate);
+}
+
+/* 앨범 수정 */
+const albumUpdateAPI = (data: any) => {
+    return axios({
+        method: 'put',
+        url: `http://${config.server.host}:${config.server.port}/album/${data.album_id}`,
+        withCredentials: true,
+        data: data,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+};
+
+function* albumUpdate(action: any): any {
+    try {
+        const result = yield call(albumUpdateAPI, action.payload);
+
+        yield put({
+            type: ALBUM_UPDATE_SUCCESS,
+            payload: result.data,
+        });
+    } catch (e) {
+        yield put({
+            type: ALBUM_UPDATE_FAILURE,
+            payload: e,
+        });
+    }
+}
+
+function* watchAlbumUpdate() {
+    yield takeEvery(ALBUM_UPDATE_REQUEST, albumUpdate);
+}
+
+/* 앨범 삭제 */
+const albumDeleteAPI = (data: any) => {
+    return axios({
+        method: 'delete',
+        url: `http://${config.server.host}:${config.server.port}/album/${data.album_id}`,
+        withCredentials: true,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+};
+
+function* albumDelete(action: any): any {
+    try {
+        const result = yield call(albumDeleteAPI, action.payload);
+        yield put({
+            type: ALBUM_DELETE_SUCCESS,
+            payload: result.data,
+        });
+    } catch (e) {
+        yield put({
+            type: ALBUM_DELETE_FAILURE,
+            payload: e,
+        });
+    }
+}
+
+function* watchAlbumeDelete() {
+    yield takeEvery(ALBUM_DELETE_REQUEST, albumDelete);
+}
 
 function* albumListSaga() {
     yield all([fork(watchAlbumListLoad)]);
 }
 
 function* albumSaga() {
-    yield all([fork(watchAlbumLoad)]);
+    yield all([fork(watchAlbumLoad), fork(watchAlbumCreate), fork(watchAlbumUpdate), fork(watchAlbumeDelete)]);
 }
 
 export { albumListSaga, albumSaga };
