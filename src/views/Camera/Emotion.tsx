8e0be55d50df23589fcb4443b01d27865848a6ed
Xxo Sio <ghdtjgml708@naver.com>
2023-03-04 03:09:50 +0900

    Feat,Fix: 실시간 차트 색상 추가, 썸네일 시간 수정

--- src/views/Camera/Emotion.tsx
+++ src/views/Camera/Emotion.tsx
@@ -1,9 +1,15 @@
 import React, { PureComponent } from 'react';
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, XAxis, Legend, ResponsiveContainer } from 'recharts';
 
 interface datas {
     name: string;
-    uv: number;
+    neutral: number;
+    happy: number;
+    sad: number;
+    surprised: number;
+    disgusted: number;
+    angry: number;
+    fearful: number;
 }
 interface data {
     data: datas[];
@@ -12,12 +18,20 @@ export default class Emotion extends PureComponent<data> {
     static defaultProps = {
         data: [{}],
     };
+
     render() {
         return (
             <ResponsiveContainer width="100%" height="20%">
-                <BarChart width={500} height={300} data={this.props.data}>
-                    <XAxis dataKey="name" />
-                    <Bar dataKey="uv" fill="#8884d8" />
+                <BarChart width={500} height={300} data={this.props.data} barGap={50}>
+                    <XAxis dataKey="Emotion" />
+                    <Bar dataKey="neutral" fill="gray" />
+                    <Bar dataKey="happy" fill="#FDBA74" />
+                    <Bar dataKey="sad" fill="#67E8F9" />
+                    <Bar dataKey="surprised" fill="#FDA4AF" />
+                    <Bar dataKey="disgusted" fill="#86EFAC" />
+                    <Bar dataKey="angry" fill="#FDE047" />
+                    <Bar dataKey="fearful" fill="#D8B4FE" />
+                    <Legend wrapperStyle={{ margin: '-1rem 0rem' }} iconSize={20} />
                 </BarChart>
             </ResponsiveContainer>
         );
--- src/views/Camera/MobileCamera.tsx
+++ src/views/Camera/MobileCamera.tsx
@@ -386,7 +386,7 @@ function MobileCamera(props: any) {
                     label: expressions.label,
                     count: 1,
                     startTime: expressions.time,
-                    maxTime: -32400000,
+                    maxTime: 0,
                 };
                 recentRecordTime = expressions.time; // 최근 감정 갱신 시간
                 mediaRecorder.start();
@@ -399,7 +399,7 @@ function MobileCamera(props: any) {
                 if (recordInfo.maxValue < expressions.value) {
                     // 최대 감정 관측 데이터 변경
                     recordInfo.maxValue = expressions.value;
-                    recordInfo.maxTime = expressions.time - recordInfo.startTime - 32400000;
+                    recordInfo.maxTime = expressions.time - recordInfo.startTime;
                 }
                 recentRecordTime = expressions.time; // 최근 감정 갱신 시간
             }
